2cf67602ced4d64d4625fe230e7529609119703e
alenakin <alenakin1@gmail.com>
2017-11-28 12:19:09 +0300

    Task6

--- Task6.Tests/CustomEnumerableTests.cs
+++ Task6.Tests/CustomEnumerableTests.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using Test6.Solution;
 
 namespace Task6.Tests
 {
@@ -16,23 +17,29 @@ namespace Task6.Tests
         {
             int[] expected = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
 
-            Assert.Inconclusive();
+            int[] result = SequanceGenerator<int>.GenerateSequance(1, 1, 10, (x, y) => x + y).ToArray();
+            CollectionAssert.AreEqual(expected, result);
         }
 
         [Test]
         public void Generator_ForSequence2()
         {
-            int[] expected = { 1, 2, -10, 92, -796, 6920, -60136, 522608, -4541680, 39469088 };
+            int[] expected = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512 };
 
-            Assert.Inconclusive();
+            int[] result = SequanceGenerator<int>.GenerateSequance(1, 2, 10, (x, y) => 6 * x - 8 * y).ToArray();
+            CollectionAssert.AreEqual(expected, result);
         }
 
         [Test]
         public void Generator_ForSequence3()
         {
-            double[] expected = { 1, 2, 3, 3.5, 4.16666666666667, 4.69047619047619, 5.29238095238095, 5.81880106357264, 6.39184849183592, 6.91728310858544 };
+            double[] expected = { 1, 2, 2.5, 3.3, 4.05757575757576, 4.87086926018965, 5.70389834408211, 6.55785277425587, 7.42763417076325, 8.31053343902137 };
 
-            Assert.Inconclusive();
+            double[] result = SequanceGenerator<double>.GenerateSequance(1, 2, 10, (x, y) => x + y / x).ToArray();
+            for (int i = 0; i < result.Count(); i++)
+            {
+                Assert.AreEqual(expected[i], result[i], 0.000000000001);
+            }
         }
     }
 }
--- Test6.Solution/SequanceGenerator.cs
+++ Test6.Solution/SequanceGenerator.cs
@@ -8,8 +8,29 @@ namespace Test6.Solution
 {
     public class SequanceGenerator<T>
     {
+        public static IEnumerable<T> GenerateSequance(T a, T b, int n, Func<T, T, T> func)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentOutOfRangeException("n must be greater than zero");
+            }
 
+            return CalculateSequance(a, b, n, func);
+        }
+
+        private static IEnumerable<T> CalculateSequance(T a, T b, int n, Func<T, T, T> func)
+        {
+            T x = a;
+            T y = b;
+            yield return x;
+
+            for (int i = 2; i <= n; i++)
+            {
+                yield return y;
+                T temp = y;
+                y = func(y, x);
+                x = temp;
+            }
+        }
     }
-
-
 }
